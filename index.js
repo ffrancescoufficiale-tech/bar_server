7139391aecc09e09175308589500382a9f8cc46b
ffrancescoufficiale-tech <ffrancescoufficiale@gmail.com>
2026-09-20 16:07:08 +0200

    Update CORS options handling in index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
-app.options('*', cors());
+app.options(/(.*)/, cors());
 app.use(express.json());
 
 // 2. RECUPERO E FORMATTAZIONE VARIABILI D'AMBIENTE
